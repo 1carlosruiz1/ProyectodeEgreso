883e5bd1a836335a20b9816ef0dcb6637c03db55
1carlosruiz1 <nathanielcarlos2005@gmail.com>
2025-08-18 01:20:46 -0300

    Update conexion.php

--- CodigoProyectoEgreso/sesion/conexion.php
+++ CodigoProyectoEgreso/sesion/conexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $con = new PDO('mysql:host=localhost;dbname=proyectoDeEgreso;charset=utf8', 'root', '');
+    $con = new PDO('mysql:host=localhost;dbname=BDProyecto;charset=utf8', 'root', '');
 } catch (PDOException $e) {
     echo json_encode(["conexion fallida" => $e->getMessage()]);
     exit;
