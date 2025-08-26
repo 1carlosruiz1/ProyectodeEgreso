56826df5e78ccdc72d7c9be56b88a00d54f35117
1carlosruiz1 <nathanielcarlos2005@gmail.com>
2025-08-25 22:23:38 -0300

    Update script_padre.sh

--- script_padre.sh
+++ script_padre.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 REPO_URL="https://github.com/1carlosruiz1/ProyectodeEgreso.git"
-PROY_DIR="/home/Gerente/Descargas"
+PROY_DIR="/usr/local/bin/proyecto-egreso"
 SCRIPTS_DIR="$PROY_DIR/scripts"
-mkdir /home/Gerente/Descargas/scripts
 
 # Borrar cualquier clon previo
 rm -rf "$PROY_DIR"
 
 # Clonar el repo completo
 git clone "$REPO_URL" "$PROY_DIR"
+
+# Asegurarse de que exista la carpeta scripts
+mkdir -p "$SCRIPTS_DIR"
 
 # Entrar a la carpeta scripts
 cd "$SCRIPTS_DIR" || { echo "No se encontró la carpeta scripts"; exit 1; }
