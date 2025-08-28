b841348c4412e46e1737c8ffc4203b6aef9235fe
1carlosruiz1 <nathanielcarlos2005@gmail.com>
2025-08-27 22:12:12 -0300

    Update script_padre.sh

--- script_padre.sh
+++ script_padre.sh
@@ -46,4 +46,45 @@ chmod +x "$DEST_DIR/$SCRIPT_NAME"
 # 9. Borrar el repositorio clonado
 rm -rf "$PROY_DIR"
 
+
+#10 ccontroles de acceso:
+
+LOG_DIR="/backup/logs"
+mkdir -p "$LOG_DIR"
+
+LOG_FILE="$LOG_DIR/registro.log"
+exec >> "$LOG_FILE" 2>&1
+echo ""
+echo "=== [$0] Inicio de ejecución: $(date) ==="
+
+# Ruta completa a menu.sh
+MENU_PATH="/usr/local/bin/scriptsProyecto/menu.sh"
+
+# Alias para Gerente
+echo "alias menu='sudo \$MENU_PATH'" >> /home/Gerente/.bashrc
+chown Gerente:Gerente /home/Gerente/.bashrc
+
+# Crear wrapper log para todos los usuarios
+cat << 'EOF_WRAPPER' > /usr/local/bin/log
+#!/bin/bash
+sudo /ruta/completa/a/menu.sh 7
+EOF_WRAPPER
+chmod +x /usr/local/bin/log
+
+chown root:root "$MENU_PATH"
+chmod 700 "$MENU_PATH"
+
+# para que todos puedan ejecutar menu.sh 7 sin contraseña
+SUDOERS_FILE="/etc/sudoers.d/menu7"
+echo "ALL ALL=(root) NOPASSWD: $MENU_PATH 7" > "$SUDOERS_FILE"
+chmod 440 "$SUDOERS_FILE"
+
+echo "Configuración completada."
+echo "Gerente puede usar 'menu'. Todos los usuarios pueden usar 'log' para punto 7."
+
+chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
+echo "Script instalado en $INSTALL_DIR/$SCRIPT_NAME con permisos de ejecución."
+
+
+
 echo "Instalación finalizada. Todos los scripts están en $DEST_DIR"
