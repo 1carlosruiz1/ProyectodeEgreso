17257ec5262112f1c23d08a1f3332f073d5460d9
1carlosruiz1 <nathanielcarlos2005@gmail.com>
2025-08-28 23:54:12 -0300

    Update alias.sh

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -1,2 +1,13 @@
+
+#!/bin/bash
+INSTALL_DIR="/etc/profile.d/"
+SCRIPT_NAME="log_access.sh"
+
+echo "Creando script de registro de accesos..."
+
+cat << 'EOF' > "$INSTALL_DIR/$SCRIPT_NAME"
 echo "alias menu='sudo /usr/local/bin/scriptsProyecto/menu.sh'" > /etc/profile.d/menu.sh
 chmod +x /etc/profile.d/menu.sh
+EOF
+chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
+echo "Script instalado en $INSTALL_DIR/$SCRIPT_NAME con permisos de ejecución."
