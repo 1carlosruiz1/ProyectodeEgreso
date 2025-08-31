250ed712cc9a9da66b3ddd51612745400f0c733f
1carlosruiz1 <nathanielcarlos2005@gmail.com>
2025-08-30 21:54:51 -0300

    Update alias.sh

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -3,20 +3,19 @@ INSTALL_DIR="/etc/profile.d/"
 SCRIPT_NAME="aliases.sh"
 LOG_DIR="/backup/logs"
 LOG_FILE="$LOG_DIR/registro.log"
-echo "Creando script de alias y variables de entorno para todos los usuarios..."
 cat << 'EOF' > "$INSTALL_DIR/$SCRIPT_NAME"
 #!/bin/bash
-# Alias y variables persistentes para todos los usuarios
 alias menu='sudo /usr/local/bin/scriptsProyecto/menu.sh'
 alias verlog='less /backup/logs/registro.log'
 export LOG_PATH="/backup/logs/registro.log"
 EOF
 chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
-sudo mkdir -p "$LOG_DIR"
-sudo touch "$LOG_FILE"
-sudo truncate -s 0 "$LOG_FILE"
-sudo chmod 644 "$LOG_FILE"
-sudo chmod 755 "$LOG_DIR"
-sudo chcon -t var_log_t "$LOG_FILE"
-echo "Script de alias instalado en $INSTALL_DIR/$SCRIPT_NAME con permisos de ejecución."
+mkdir -p "$LOG_DIR"
+touch "$LOG_FILE"
+truncate -s 0 "$LOG_FILE"  # vacía el archivo al inicio
+chmod 755 "$LOG_DIR"      # todos pueden entrar al directorio
+chmod 644 "$LOG_FILE"     # todos pueden leer el log, solo root puede escribir
+# SELinux)
+chcon -t var_log_t "$LOG_DIR"
+chcon -t var_log_t "$LOG_FILE"
 echo "Alias 'menu' y 'verlog' disponibles para todos los usuarios al iniciar sesión."
