a92cc4911e10b65bc22580244fbfd9c537d685aa
1carlosruiz1 <nathanielcarlos2005@gmail.com>
2025-09-03 07:47:08 -0300

    Update tablas.sql

--- tablas.sql
+++ tablas.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS BDProyecto;
-Create database BDProyecto;
+CREATE DATABASE BDProyecto;
 USE BDProyecto;
-CREATE TABLE Usuarios (
+CREATE TABLE Usuario (
     ID_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
@@ -10,29 +10,25 @@ CREATE TABLE Usuarios (
     activo BOOLEAN DEFAULT TRUE
 );
 
-
 CREATE TABLE Cliente (
     ID_cliente INT PRIMARY KEY,
     PuntosCliente INT DEFAULT 0,
-    FOREIGN KEY (ID_cliente ) REFERENCES Usuarios(ID_usuario)
+    FOREIGN KEY (ID_cliente) REFERENCES Usuario(ID_usuario)
 );
 
-
 CREATE TABLE EtiquetaCliente (
-    ID_cliente INT NOT NULL,
-    tipoEtiqueta ENUM('no-shows', 'problematico', 'no confirma') NOT NULL,
+    ID_cliente INT,
+    tipoEtiqueta ENUM('no-shows', 'problematico', 'no confirma'),
     fechaCalificacion DATE,
-    comentario TEXT,
+    comentario VARCHAR(255),
     PRIMARY KEY (ID_cliente, tipoEtiqueta, fechaCalificacion),
     FOREIGN KEY (ID_cliente) REFERENCES Cliente(ID_cliente)
 );
 
-
 CREATE TABLE Empleado (
     ID_empleado INT PRIMARY KEY,
-    FOREIGN KEY (ID_empleado) REFERENCES Usuarios(ID_usuario)
+    FOREIGN KEY (ID_empleado) REFERENCES Usuario(ID_usuario)
 );
-
 
 CREATE TABLE Delivery (
     ID_delivery INT PRIMARY KEY,
@@ -41,123 +37,112 @@ CREATE TABLE Delivery (
 
 CREATE TABLE Cocinero (
     ID_cocinero INT PRIMARY KEY,
-    FOREIGN KEY (ID_cocinero ) REFERENCES Empleado(ID_empleado)
+    FOREIGN KEY (ID_cocinero) REFERENCES Empleado(ID_empleado)
 );
 
 CREATE TABLE GerenteGeneral (
     ID_gerente INT PRIMARY KEY,
-    ID_empleado INT NOT NULL,
-    FOREIGN KEY (ID_gerente ) REFERENCES Empleado(ID_empleado)
+    FOREIGN KEY (ID_gerente) REFERENCES Empleado(ID_empleado)
 );
 
 CREATE TABLE ChefEjecutiva (
     ID_chef INT PRIMARY KEY,
-    FOREIGN KEY (ID_chef ) REFERENCES Empleado(ID_empleado)
+    FOREIGN KEY (ID_chef) REFERENCES Empleado(ID_empleado)
 );
 
 CREATE TABLE Mozo (
     ID_mozo INT PRIMARY KEY,
-    FOREIGN KEY (ID_mozo ) REFERENCES Empleado(ID_empleado)
+    FOREIGN KEY (ID_mozo) REFERENCES Empleado(ID_empleado)
 );
 
+CREATE TABLE Mesa (
+    ID_mesa INT AUTO_INCREMENT PRIMARY KEY,
+    numMesa INT
+);
 
 CREATE TABLE Pedido (
     ID_pedido INT AUTO_INCREMENT PRIMARY KEY,
-    ID_cliente INT NOT NULL,
-    ID_mozo INT NOT NULL,
-    pedidoCocinado BOOLEAN DEFAULT FALSE,
-    fechaPedido DATETIME NOT NULL,
-    especificaciones TEXT,
+    ID_cliente INT,
+    ID_mozo INT,
+    pedidoCocinado BOOLEAN,
+    fechaPedido DATETIME,
+    especificaciones VARCHAR(255),
     fechaEsperadaUsuario DATETIME,
-    puntosCompraUsuario INT DEFAULT 0,
+    puntosCompraUsuario INT,
     numMesaRestaurante INT,
     FOREIGN KEY (ID_cliente) REFERENCES Cliente(ID_cliente),
-    FOREIGN KEY (ID_mozo) REFERENCES Mozo(ID_mozo)
+    FOREIGN KEY (ID_mozo) REFERENCES Mozo(ID_mozo),
+    FOREIGN KEY (numMesaRestaurante) REFERENCES Mesa(ID_mesa)
 );
-
 
 CREATE TABLE Entrega (
     ID_entrega INT AUTO_INCREMENT PRIMARY KEY,
-    ID_pedido INT NOT NULL,
-    ID_delivery INT NOT NULL,
-    confirmEntregaUsuario BOOLEAN DEFAULT FALSE,
-    confirmEntregaDelivery BOOLEAN DEFAULT FALSE,
+    ID_pedido INT,
+    ID_delivery INT,
+    confirmEntregaUsuario BOOLEAN,
+    confirmEntregaDelivery BOOLEAN,
     ubicacionUsuario VARCHAR(255),
     FOREIGN KEY (ID_pedido) REFERENCES Pedido(ID_pedido),
     FOREIGN KEY (ID_delivery) REFERENCES Delivery(ID_delivery)
 );
 
-
 CREATE TABLE Plato (
     ID_plato INT AUTO_INCREMENT PRIMARY KEY,
-    Precio DECIMAL(10,2) NOT NULL,
+    nombre_plato VARCHAR(100),
+    precio DECIMAL(10,2),
     temporada VARCHAR(50),
-    tiempoPreparacion INT
+    tiempoPreparacion INT,
+    descripcion VARCHAR (500)
 );
-
 
 CREATE TABLE Ingrediente (
     ID_ingrediente INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL
-);
-
-
-CREATE TABLE StockIngrediente (
-    ID_ingrediente INT NOT NULL,
-    cantidad DECIMAL(10,2) NOT NULL,
+    nombre VARCHAR(100),
+    cantidad INT,
     fechaVencimiento DATE,
-    fechaIngreso DATE,
-    PRIMARY KEY (ID_ingrediente, fechaIngreso),
-    FOREIGN KEY (ID_ingrediente) REFERENCES Ingrediente(ID_ingrediente)
-);
+    fechaIngreso DATE
+    );
 
 CREATE TABLE Gastan (
-    ID_plato INT NOT NULL,
-    ID_ingrediente INT NOT NULL,
-    cantidadIngrediente DECIMAL(10,2),
+    ID_plato INT,
+    ID_ingrediente INT,
+    cantidadIngrediente INT,
     magnitud VARCHAR(50),
     PRIMARY KEY (ID_plato, ID_ingrediente),
     FOREIGN KEY (ID_plato) REFERENCES Plato(ID_plato),
     FOREIGN KEY (ID_ingrediente) REFERENCES Ingrediente(ID_ingrediente)
 );
 
-
 CREATE TABLE SeCompone (
-    ID_plato INT NOT NULL,
-    ID_pedido INT NOT NULL,
-    cantidad INT DEFAULT 1,
+    ID_plato INT,
+    ID_pedido INT,
+    cantidad INT,
     PRIMARY KEY (ID_plato, ID_pedido),
     FOREIGN KEY (ID_plato) REFERENCES Plato(ID_plato),
     FOREIGN KEY (ID_pedido) REFERENCES Pedido(ID_pedido)
 );
 
-
-CREATE TABLE Mesa (
-    ID_mesa INT AUTO_INCREMENT PRIMARY KEY,
-    numMesa INT NOT NULL
-);
-
-
 CREATE TABLE Reserva (
     ID_reserva INT AUTO_INCREMENT PRIMARY KEY,
-    ID_mesa INT NOT NULL,
-    horaInicio DATETIME NOT NULL,
-    horaFin DATETIME NOT NULL,
+    ID_mesa INT,
+    horaInicio DATETIME,
+    horaFin DATETIME,
     confirmado BOOLEAN,
     cancelado BOOLEAN,
-    fechaReserva DATE NOT NULL,
+    fechaReserva DATE,
     fechaRealizacionCliente DATE,
     FOREIGN KEY (ID_mesa) REFERENCES Mesa(ID_mesa)
 );
 
 CREATE TABLE Comentario (
     ID_comentario INT AUTO_INCREMENT PRIMARY KEY,
-    ID_cliente INT NOT NULL,
-    informacion TEXT,
-    puntaje INT CHECK(puntaje BETWEEN 0 AND 5),
-    fecha DATETIME NOT NULL,
+    ID_cliente INT,
+    informacion VARCHAR(255),
+    puntaje INT,
+    fecha DATE,
     FOREIGN KEY (ID_cliente) REFERENCES Cliente(ID_cliente)
 );
+
 
 
 
