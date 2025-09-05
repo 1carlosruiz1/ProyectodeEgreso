d2da0d25983cb4261e5f0dc39061f43636bf5393
1carlosruiz1 <nathanielcarlos2005@gmail.com>
2025-09-04 21:03:47 -0300

    Delete CodigoProyectoEgreso directory

--- CodigoProyectoEgreso/CodigoProyectoEgreso/sesion/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $con = new PDO('mysql:host=localhost;dbname=BDProyecto;charset=utf8', 'root', '');
-} catch (PDOException $e) {
-    echo json_encode(["conexion fallida" => $e->getMessage()]);
-    exit;
-}
