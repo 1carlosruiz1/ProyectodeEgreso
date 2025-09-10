d500bb7688c9fdc89a103f62be42b42b0d321ac3
1carlosruiz1 <nathanielcarlos2005@gmail.com>
2025-09-09 21:20:03 -0300

    Add files via upload

---
+++ CodigoProyectoEgreso/Cliente/menu/datos.php
@@ -0,0 +1,14 @@
+<?php
+include '../sesion/conexion.php';
+
+$sql = "SELECT * FROM plato";
+$stmt = $con->prepare($sql);
+$stmt->execute();
+$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if (!$res) {
+       echo json_encode(["error" => "no hay datos"]);
+
+} else {
+       echo json_encode(["success" => $res]);
+
+}
